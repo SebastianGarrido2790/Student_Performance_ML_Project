c6f9b7d9fe490808c34b63ae2c050f673b29c6c1
Sebastian <sebastiangarrido2790@gmail.com>
2025-02-13 16:19:02 -0300

    logging and exception

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setup.tools import find_packages, setup
+from typing import List
+
+
+HYPEN_E_DOT = (
+    "-e ."  # This is the string that we want to remove from the requirements.txt file
+)
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+
+setup(
+    name="Student-Performance-ML-Project",
+    version="0.0.1",
+    author="Sebastian Garrido",
+    author_email="sebastiangarrido2790@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
